5a7871eecb8dbb8c1470c767fa182bacb9595b49
jbrownssf <jpbrown@softstackfactory.org>
2017-05-08 09:44:24 +0000

    tbc

--- Farkle10K/src/pages/game/game.ts
+++ Farkle10K/src/pages/game/game.ts
@@ -15,32 +15,43 @@ import { Gamelobby } from '../gamelobby/gamelobby';
 })
 export class Game {
   showHome: boolean = false;
-  die1: object = {};
+  // die1: object = {}; //from prototyping / testing
   dice: any = [];
   scores: any = [];
   highScores: any;
+  countingDice: array = [];
+  runningTotal: any;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.dice = [
         {
           "value": "F",
-          "selected": false
+          "selected": false,
+          "counted": false
         },{
           "value": "A",
-          "selected": false
+          "selected": false,
+          "counted": false
         },{
           "value": "R",
-          "selected": false
+          "selected": false,
+          "counted": false
         },{
           "value": "K",
-          "selected": false
+          "selected": false,
+          "counted": false
         },{
           "value": "L",
-          "selected": false
+          "selected": false,
+          "counted": false
         },{
           "value": "E",
-          "selected": false
-        }
+          "selected": false,
+          "counted": false
+        } // index used to reference die position, value 1-6, selected for click, 
+          // and counted for scoring and to prevent last round's selected dice from being toggled
       ]; //array of objects {"value": 1-6, "selected": false}
+    //prepopulated with scores for view testing purposes
+    //this array will eventually hold the turn by turn scores (should be saved when game state is saved)
     this.scores = [
         {
         'player': 0,
@@ -62,15 +73,26 @@ export class Game {
         'CPU': 6900
         }
       ];
+      //this is so that the current score can be seen - might be a betterway, but it will
+      //also align with the highscore leader board later on, so we'll see
       this.highScores = {
         'player': 10000,
         'CPU': 6900
       };
+      this.countingDice = [
+        {"ones": 0},
+        {"twos": 0},
+        {"threes": 0},
+        {"fours": 0},
+        {"fives": 0},
+        {"sixes": 0}
+      ]; //helps us score each set of dice - counts how many of each face value we're working with
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad Game');
       this.showHome = this.navParams.get("showHome");
+      //temporary intialization of scores
       this.scores[0]['player'] = 0;
       this.scores[0]['CPU'] = 0;
   }
@@ -86,6 +108,7 @@ export class Game {
   // }
   //when a die is clicked from the page - this toggles the selection in the dice array
   selectDie(die){
+    if(!this.dice[die]['counted'])
     this.dice[die]['selected'] = !this.dice[die]['selected'];
   }
   //this will roll new values for any Un-Selected Dice in the dice array
@@ -96,7 +119,33 @@ export class Game {
       } 
     }
   }
-  
+  countEm(){
+    //this will subtotal the selected dice from each re-roll
+    //first, we iterate through all dice
+    this.dice.forEach(die){
+      //if a die is selected, and hasn't been counted
+      if (die.selected && !die.counted){
+        switch(die){
+          //iterate the counter of countingDice
+          case 1: this.countingDice['ones']++; break;
+          case 2: this.countingDice['twos']++; break;
+          case 3: this.countingDice['threes']++; break;
+          case 4: this.countingDice['fours']++; break;
+          case 5: this.countingDice['fives']++; break;
+          case 6: this.countingDice['sixes']++; break;
+        }
+        //this way we don't accidentaly let people score triples off of dice rolled seperately
+        this.dice[die]['counted'] = true;
+      }
+    };
+  }
+  bankIt(){
+    //we need to score this game somehow
+    //Okay, so on a very basic level take the values of the six dice - as an array?
+    //then we can do a switch case on each die face value - so that singles, sets, etc get caught 
+    //maybe cascade from 6 of a kind down ?
+    
+  }
   //return a value between 1 and 6
   rollD6(){
     return Math.floor(Math.random() * 6) + 1;
